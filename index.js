eae7f30bc98480f5985dbac9fad88d1ca2d78fcc
Michael Zapata <michael.zapata@scality.com>
2016-02-06 16:48:24 +0100

    cleanup: follow guidelines closely

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 module.exports = {
-    "extends": "airbnb/base",
+    extends: "airbnb/base",
 
-    "env": {
-        "browser": false,
-        "node": true,
-        "mocha": true
+    env: {
+        browser: false,
+        node: true,
+        mocha: true,
     },
 
-    "rules": {
+    rules: {
         "strict": [2, "global"],
         "indent": [2, 4],
         "no-mixed-spaces-and-tabs": 2,
@@ -16,18 +16,18 @@ module.exports = {
         "semi": [2, "always"],
         "curly": 0,
         "space-before-keywords": 2,
-        "space-before-function-paren": [2, {"anonymous": "always",
-                                            "named": "never"}],
+        "space-before-function-paren": [2, { anonymous: "always",
+                                             named: "never"} ],
         "comma-dangle": 1,
-        "camelcase": [2, {"properties": "always"}],
+        "camelcase": [2, { properties: "always" }],
         "id-length": 0,
         "no-shadow": 0,
         "valid-jsdoc": 1,
         "eol-last": 2,
-        "no-multiple-empty-lines": [ 2, { "max": 2, "maxEOF": 1 } ],
+        "no-multiple-empty-lines": [ 2, { max: 2, maxEOF: 1 } ],
         "no-console": 2,
         "object-shorthand": 1,
         "quote-props": [1, "consistent-as-needed"],
         "prefer-template": 1,
-    }
+    },
 };
