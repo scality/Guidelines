241de0d14cc17e189409341626813bced00ab20e
Michael Zapata <michael.zapata@scality.com>
2016-03-14 14:48:30 +0100

    feat(linter): add new lints to cover

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ module.exports = {
         "indent": [2, 4],
         "no-mixed-spaces-and-tabs": 2,
         "max-len": [2, 80, 4],
-        "quotes": 0,
+        "quotes": [1, "single", "avoid-escape"],
         "semi": [2, "always"],
         "curly": 0,
         "space-before-keywords": 2,
@@ -29,5 +29,18 @@ module.exports = {
         "object-shorthand": 1,
         "quote-props": [1, "consistent-as-needed"],
         "prefer-template": 1,
+        "array-bracket-spacing": [1, "never"],
+        "keyword-spacing": [1, {
+            before: true,
+            after: true,
+            overrides: {
+                return: { after: true },
+                throw: { after: true },
+                case: { after: true },
+            },
+        }],
+        "linebreak-style": [1, "unix"],
+        "new-cap": [1, { newIsCap: true }],
+        'space-in-parens': [1, 'never'],
     },
 };
