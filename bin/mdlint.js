a44d1aadbee2e78e21b9502f11eb5c5843f0dac9
Naren <narendran.rajendran@scality.com>
2022-01-26 16:08:59 -0800

    improvement: GDL-5 linter corrections

--- bin/mdlint.js
+++ bin/mdlint.js
@@ -6,13 +6,13 @@ const files = commander.parse(process.argv).args;
 
 // See rules at https://github.com/mivok/markdownlint/blob/master/docs/RULES.md
 const config = {
-    MD004: false,  // Unordered list style
-    MD007: false,  // Unordered list indentation
-    MD024: false,  // Multiple headers with the same content
-    MD027: false,  // Multiple spaces after blockquote symbol
+    MD004: false, // Unordered list style
+    MD007: false, // Unordered list indentation
+    MD024: false, // Multiple headers with the same content
+    MD027: false, // Multiple spaces after blockquote symbol
     MD029: { style: 'ordered' }, // Ordered list item prefix
-    MD034: false,  // Bare URL used
-    MD040: false,  // Fenced code blocks should have a language specified
+    MD034: false, // Bare URL used
+    MD040: false, // Fenced code blocks should have a language specified
 };
 
 const result = markdownlint.sync({ files, config });
